34aad22c0a6563567920532da6a4327434add17b
Tsunami43 <n3m0_22@mail.ru>
2026-01-04 02:21:10 +0300

    update install for firefox

--- firefox/install.sh
+++ firefox/install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-PROFILE_DIR=$(ls -d "$HOME/Library/Application Support/Firefox/Profiles/"*default-release 2>/dev/null | head -n1)
+PROFILES_DIR="$HOME/Library/Application Support/Firefox/Profiles"
 
-if [ -z "$PROFILE_DIR" ]; then
-    echo "Error: profile Firefox not found!"
+found=false
+
+for PROFILE_DIR in "$PROFILES_DIR"/*; do
+    [ -d "$PROFILE_DIR" ] || continue
+
+    found=true
+    echo "Installing config into: $PROFILE_DIR"
+
+    cp -r user.js "$PROFILE_DIR/"
+    cp -r chrome/ "$PROFILE_DIR/"
+done
+
+if [ "$found" = false ]; then
+    echo "Error: Firefox profiles not found!"
     exit 1
 fi
 
-cp -r user.js "$PROFILE_DIR/"
-cp -r chrome/ "$PROFILE_DIR/"
-echo "Installed.."
+echo "Installed into all profiles."
