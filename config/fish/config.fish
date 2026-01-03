d020b5ba4291714718241e94f130260b5b3df692
Tsunami43 <n3m0_22@mail.ru>
2026-01-03 19:53:21 +0300

    update fish

---
+++ config/fish/config.fish
@@ -0,0 +1,48 @@
+if status is-interactive
+    # RUN on session sh
+end
+
+# PATH
+set -gx PATH $HOME/bin $PATH
+set -gx PATH $PATH (go env GOPATH)/bin
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.pyenv/shims $PATH
+
+# Editor
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx GIT_EDITOR nvim
+
+# Starship prompt
+set -gx STARSHIP_CONFIG $HOME/.config/starship/starship.toml
+starship init fish | source
+
+# Alias
+alias vi="nvim"
+alias vim="nvim"
+alias neofetch="fastfetch"
+alias ls="eza -l --tree --level=1 --icons=always --no-user --no-permissions"
+
+# Tmux
+function tmux
+    if test (count $argv) -eq 0
+        set NAMES_FILE $HOME/.config/tmux/session_names.txt
+        set LINES (cat $NAMES_FILE)
+        set SESSION_NAME $LINES[(math "1 + random % " (count $LINES))]
+        command tmux new-session -s $SESSION_NAME
+    else
+        command tmux $argv
+    end
+end
+
+# Homebrew
+set -gx HOMEBREW_NO_ENV_HINTS 1
+
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
